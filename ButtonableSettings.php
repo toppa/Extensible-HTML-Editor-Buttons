07ae9038e886a0b3b32f8be1c8d39548e505892b
Michael Toppa <public@toppa.com>
2013-02-10 13:52:51 -0500

    use Toppa Libs locally, instead of loading externally

--- ButtonableSettings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ButtonableSettings extends ToppaSettingsWp {
-    public function __construct(ToppaFunctionsFacade $functionsFacade) {
-        parent::__construct('buttonable', $functionsFacade);
-    }
-}
---
+++ lib/ButtonableSettings.php
@@ -0,0 +1,88 @@
+<?php
+
+class ButtonableSettings {
+    private $functionsFacade;
+    private $name = 'buttonable';
+    private $data = array();
+
+    public function __construct(ButtonableFunctionsFacade $functionsFacade) {
+        $this->functionsFacade = $functionsFacade;
+    }
+
+    public function __get($name) {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        else {
+            $this->refresh();
+        }
+
+        // and try again...
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        throw New Exception(__('Invalid data property __get for ', 'toppalibs') . htmlentities($name));
+    }
+
+    public function refresh() {
+        $currentSettings = $this->functionsFacade->getSetting($this->name);
+
+        if (is_array($currentSettings)) {
+            $this->data = $currentSettings;
+        }
+
+        return $this->data;
+    }
+
+    public function set(array $newSettings, $preferExisting = false) {
+        $this->refresh();
+
+        if ($preferExisting) {
+            $this->data = ToppaFunctions::arrayMergeRecursiveForSettings($newSettings, $this->data);
+        }
+
+        else {
+            $this->data = ToppaFunctions::arrayMergeRecursiveForSettings($this->data, $newSettings);
+        }
+
+        $this->functionsFacade->setSetting($this->name, $this->data);
+        return $this->data;
+    }
+
+    public function delete() {
+        $this->functionsFacade->deleteSetting($this->name);
+
+        if ($this->functionsFacade->getSetting($this->name)) {
+            throw new Exception(__('Failed to delete settings', 'toppalibs'));
+        }
+
+        return true;
+    }
+
+    // the set method merges old and new settings, which means you can't
+    // use it to delete settings data. Purge will remove the
+    // specified settings element, up to 3 levels deep
+    public function purge(array $settingToRemove) {
+        $this->refresh();
+
+        if (count($settingToRemove) == 1) {
+            unset($this->data[$settingToRemove[0]]);
+        }
+        elseif (count($settingToRemove) == 2) {
+            unset($this->data[$settingToRemove[0]][$settingToRemove[1]]);
+        }
+        elseif (count($settingToRemove) == 3) {
+            unset($this->data[$settingToRemove[0]][$settingToRemove[1]][$settingToRemove[2]]);
+        }
+        else {
+            throw new Exception(__('Failed to purge settings. Unsupport argument provided.', 'toppalibs'));
+        }
+
+        $this->functionsFacade->setSetting($this->name, $this->data);
+        return $this->data;
+    }
+}
+
+
